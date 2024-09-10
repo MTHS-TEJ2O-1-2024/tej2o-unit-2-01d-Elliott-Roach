d304d9b6c574115de36e4913c72e1b8806835448
Elliott-Roach <elliott.roach@mths.ca>
2024-09-10 23:01:23 +0000

    addid what program dose

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,7 +1,7 @@
 """
 Created by: Elliott
 Created on: Sep 2024
-This module is a Micro:bit MicroPython program
+This module is a Micro:bit MicroPython program. says hello elliott!
 """
 
 from microbit import *
